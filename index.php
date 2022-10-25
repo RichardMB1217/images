0da01b7c79d6752cd2bd8ef2ca51a2cf34a60815
RichardMB1217 <116602303+RichardMB1217@users.noreply.github.com>
2022-10-24 22:19:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,9 @@
     <link rel="stylesheet" type="text/css" href="inc/ins-media.css">
 </head>
 <body>
-    <header><div><h1>Show all media in my folder</h1></div></header>
-
+    
     <!-- media insertion -->
     <?php include "inc/ins-media.php"; ?>
 
-    <footer><div><?php echo date("Y"); ?> // source code <a target="_blank" href="https://github.com/dvdn/show-all-media-in-a-folder-with-php">dvdn/show-all-media-in-a-folder-with-php</a></div></footer>
 </body>
 </html>
